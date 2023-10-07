4b0fa11c1387c0888eb4c550b3e04d940bf88792
cyberfly5 <cyberfly5@yahoo.com>
2023-10-07 22:52:46 +0000

    fixed backend, notifications and home ok

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -154,7 +154,7 @@ def data_create_message():
 @app.route("/api/activities/home", methods=['GET'])
 @xray_recorder.capture('activities_home')
 def data_home():
-  data = HomeActivities.run(Logger=LOGGER)
+  data = HomeActivities.run()
   return data, 200
 
 @app.route("/api/activities/notifications", methods=['GET'])
